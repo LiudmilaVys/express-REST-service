0c4da400041928e7ec12b5cc32f0037881577e14
Liudmila Kurytskaya <liudmila.kurytskaya@thomsonreuters.com>
2020-10-14 17:32:37 +0300

    handle process errors

--- src/app.js
+++ src/app.js
@@ -9,15 +9,28 @@ const taskRouter = require('./resources/tasks/task.router');
 const userRouter = require('./resources/users/user.router');
 const logger = require('./common/logger');
 
+// To check this please uncomment line 18
+process.on('uncaughtException', error => {
+  logger.error('Process error:', { message: error.message }, error, () =>
+    process.exit(1)
+  );
+});
+//throw Error('Oops!');
+
+// To check this please uncomment line 24
+process.on('unhandledRejection', message => {
+  logger.error('Unhandled rejection:', { message });
+});
+//Promise.reject('Async operation has failed.');
+
 db.init();
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
-
 app.use((req, res, next) => {
-  \\throw new Error('Some server error.');
+  //throw new Error('Some service error.');
   const { method, url, query, body } = req;
   logger.info(
     `method: ${method}, URL: ${url}, query: ${JSON.stringify(
@@ -30,15 +43,13 @@ app.use((req, res, next) => {
   );
   next();
 });
-
-// To check this please uncomment line 20
+// To check this please uncomment line 33
 app.use((err, req, res, next) => {
-  logger.error(err.message);
+  logger.error(`App error: ${err.message}`, err.message);
   res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
 });
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -46,7 +57,6 @@ app.use('/', (req, res, next) => {
   }
   next();
 });
-
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
 app.use('/users', userRouter);
